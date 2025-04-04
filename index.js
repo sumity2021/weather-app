9c90dc63d956f5f53e50e04da900268e375c4088
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-04-04 21:05:57 +0000

    error fix

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  let data = await response.json();
-  console.log(data);
+  if(response.status==404){
+    document.querySelector(".error").style.display="block";
+    document.querySelector(".weather").style.display="none";
+  }
+  else{
+    let data = await response.json();
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
@@ -24,6 +28,9 @@ async function checkWeather(city) {
   } else if (data.weather[0].main == "Clear") {
     weatherIcon.src = "./images/clear.png";
   }
+  document.querySelector(".weather").style.display="block";
+  document.querySelector(".error").style.display="none";
+}
 }
 
 searchBtn.addEventListener("click", () => {
